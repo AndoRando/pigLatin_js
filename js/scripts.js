aa95932a409ee85ca833d13193bd6a19b7162ea0
Andrew Brennwald & Ryan Heupler <student@epicodus.com>
2015-09-09 10:22:17 -0700

    Implement functionality for full sentences with punctuation.

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,22 @@
 // var pigLatin = function(word) {
 //   vowels = ["a", "e", "i", "o", "u"];
-//   if(vowels.indexOf(word.charAt(0)) !== -1) {
-//     return word + "ay";
-//   } else {
-//     word.slice(1) + word.slice(0,1);
-//
+//   while (vowels.indexOf(word.charAt(0)) === -1) {
+//     word = word.slice(1) + word.slice(0,1);
 //   }
+//   return word + "ay";
 // };
 
 
-var pigLatin = function(word) {
-  vowels = ["a", "e", "i", "o", "u"];
-  while (vowels.indexOf(word.charAt(0)) === -1) {
-    word = word.slice(1) + word.slice(0,1);
+var pigLatin = function(sentence) {
+  var vowels = ["a", "e", "i", "o", "u"];
+  var split_sentence = sentence.toLowerCase().split(/\W\s*/);
+  var pig_sentence = [];
+
+  for (var i = 0; i < split_sentence.length; i++) {
+    while (vowels.indexOf(split_sentence[i].charAt(0)) === -1) {
+      split_sentence[i] = split_sentence[i].slice(1) + split_sentence[i].slice(0,1);
+    }
+    pig_sentence.push(split_sentence[i] + "ay");
   }
-  return word + "ay";
+  return pig_sentence.join(" ");
 };
--- spec/specs.js
+++ spec/specs.js
@@ -1,7 +1,13 @@
 describe('pigLatin', function() {
-  it("attaches 'ay' to end of word", function() {
-    expect(pigLatin("apple")).to.eq("appleay");
-  });
+  // it("attaches 'ay' to end of word", function() {
+  //   expect(pigLatin("apple")).to.eq("appleay");
+  // });
+  //
+  // it("moves first consecutive consonants to the end of word", function() {
+  //   expect(pigLatin("chello")).to.eq("ellochay");
+  // });
 
-  it("")
+  it("converts sentences into pig latin", function() {
+    expect(pigLatin("Yo Frodo, go into Mordor")).to.eq("oyay odofray ogay intoay ordormay");
+  });
 });
